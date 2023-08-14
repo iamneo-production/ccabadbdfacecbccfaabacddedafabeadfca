3855d0edf1df3e2cc09cb0074fe27c30d4fdf741
Iamneo <techadmin@iamneo.ai>
2023-08-14 12:48:27 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -3,7 +3,9 @@ package ai.iamneo.testing.Testing_Selenium_TestNg;
 import org.testng.annotations.Test;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
@@ -42,13 +44,16 @@ public class AppTest {
 //Moving to FACEBOOK
 	public void testcase_2() throws InterruptedException 
 	{
-		driver.findElement(By.id("gh-ac")).sendKeys(product);
+		driver.findElement(By.id("gh-ac")).sendKeys("Apple Watch");
 		driver.findElement(By.id("gh-btn")).click();
 		String get = driver.findElement(By.xpath("[@id=\"srp-river-results\"]/ul/li[1]/div/div[2]/a/div/span")).getText();
 		Assert.assertEquals(get,"Apple Watch Series 5 40mm 44mm GPS+ WIFI +LTE UNLOCKED Gold Gray Silver - Good");
 		
 
 	}
+	/**
+	 * @throws InterruptedException
+	 */
 	@Test
 //Back to iamNeo
 	public void testcase_3() throws InterruptedException 
